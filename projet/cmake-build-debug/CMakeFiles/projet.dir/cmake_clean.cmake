42633e4a2ab030d0c747f2dc83dec620ebef2be1
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2019-03-07 16:14:30 +0100

    Exov1

---
+++ projet/cmake-build-debug/CMakeFiles/projet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet.dir/main.c.obj"
+  "projet.pdb"
+  "projet.exe"
+  "projet.exe.manifest"
+  "libprojet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
