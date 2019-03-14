7f6dce2b50257aed25426b0f20cdb8e2c63a6d24
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2019-03-14 10:21:52 +0100

    readme update

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-7a DESCRIPTIF DU PROJET
-	7.1	Généralités
-Ce module MA-20 a pour objectifs d'entraîner les étudiants à la réalisation de projet de type développement.
-7.2	Fonctionnalités minimales
-Le type d’application C demandé doit offrir les fonctionnalités minimales suivantes :
-Version 0.1 (pour le 2 mars 2018) :
-•	Jouer à la bataille navale alors que la position des bateaux est codée dans le programme
-•	Afficher l’aide du jeu
-Version 1.0 (pour le 20 avril 2018)
-•	S’authentifier en tant que joueur
-•	Logger les faits importants durant toute la durée de fonctionnement de l’application
-•	Le programme choisit une grille au hasard dans un répertoire et la charge 
-•	Afficher la liste des scores atteints lors des parties précédentes
-Notes : 
-•	Des fichiers seront utilisés pour la persistance des données.
-•	Un menu de type console sera utilisé pour accéder aux différentes fonctionnalités.
-•	Une extension pourra être envisagée pour ceux qui auraient terminé toutes les fonctionnalités avant la fin du temps imparti. Elle sera à discuter entre l’élève et le mandant du projet.
-7b POINTS ÉVALUÉS DURANT LE PROJET
-●	Le processus « Analyse préliminaire – Conception – Implémentation »
-●	Le code doit être correctement commenté
-●	Le code doit être bien structuré : il comprendra notamment plusieurs fonctions
-●	Les cas d’erreur doivent être traités : messages d’erreur (pas de plantées)
-●	Régularité dans l’effort (livrable chaque semaine)
-●	Qualité des messages accompagnant les commits
+# BataileNaval
+
+## Description
+Bataile Navale c'est un projet coder en C type de projet un jeu    
+
+## Génération
+pour exécuter le jeu les requis besion :
+- Télécharger le dossier projet  
+- Editeur de langage C  Clion
+- Cmd invte à commander Optinal
+
+
+
+
+##Pour générer l'exécutable, vous devez:
+
+1. ouvrir le dossier avec Clion 
+1. compiler le programme 
+1. l'exutable  se trouvra dans le dossier cmake-build-debug
+1. Duble clique pour le lancer 
+
+## Exécution
+
+Pour exécuter le programme, vous devez:
+
+- voir le génération 
--- projet/main.c
+++ projet/main.c
@@ -1,28 +1,63 @@
+/*Prénom:marwan
+ * nom:alhelo
+ * date:08.03.2018
+ * titre:bataille navale
+ *
+ */
 #include <stdio.h>
 #include <windows.h>
+
+#define STLC 218 // ┌, Single Top Left Corner
+#define STRC 191 // ┐, Single Top Right Corner
+#define SBLC 192 // └, Single Bottom Left Corner
+#define SBRC 217 // ┘, Single Bottom Right Corner
+#define SVSB 179 // │, Single Vertical Simple Border
+#define SVRB 180 // ┤, Single Vertical Right Border
+#define SVLB 195 // ├, Single Vertical Left Border
+#define SHSB 196 // ─, Single Horizontal Simple Border
+#define SHBB 193 // ┴, Single Horizontal Bottom Border
+#define SHTB 194 // ┬, Single Horizontal Top Border
+#define SC   197 // ┼, Single Center
+
 #pragma execution_character_set("utf-8")
+
+ void grillevid() {
+     printf("voilà la grille!\n");
+     printf ("Une grille basée sur les lignes simples:\n");
+     SetConsoleOutputCP(437); // For semi-graphic characters
+     for(int i=0; i<=10; i++){
+         printf ("%c%c%c%c%c%c%c%c%c\n",STLC,SHSB,SHSB,SHSB,SHTB,SHSB,SHSB,SHSB,STRC);
+         printf ("%c   %c   %c\n",SVSB,SVSB,SVSB);
+         printf ("%c%c%c%c%c%c%c%c%c\n",SVLB,SHSB,SHSB,SHSB,SC,SHSB,SHSB,SHSB,SVRB);
+         printf ("%c   %c   %c\n",SVSB,SVSB,SVSB);
+         printf ("%c%c%c%c%c%c%c%c%c\n",SBLC,SHSB,SHSB,SHSB,SHBB,SHSB,SHSB,SHSB,SBRC);
+     }
+
+ }
 int main() {
     SetConsoleOutputCP(65001);
-    int choix;
-    printf(" \n1.Commence jouer\n2.Affiche d'aide\n3.par rapport le jeu\nvotre choix:\n");
-    scanf("%d",&choix);
+    int choix=1;
 
-while(choix!=1&&choix!=2&&choix!=3){
-    printf("entrez les chiffre entre 1 et 3:\n");
-    scanf("%d",&choix);
+    while (choix != 0) {
+        printf(" \n1.Commence jouer\n2.Affiche d'aide\n3.par rapport le jeu\n0. Quitter \nvotre choix:\n");
+        scanf("%d", &choix);
 
-}
-if(choix==3){
-    printf("C'est un jou simple pour il faut attaquer les ennmies et puis survivre ");
+        if (choix == 3) {
 
-}
-else if (choix==2){
-    printf("comment jouer ?\n il faut regarder sur la grilles et savoir votre postistion et la position de votre énnmie aussi puis bouuuf attaquer");
+            printf("C'est un jou simple pour il faut attaquer les ennmies et puis survivre ");
 
-}
-else{
-    printf("on est gooo !");
+        } else if(choix==1){
+          grillevid();
+        }
+        else if (choix == 2) {
+            printf("comment jouer ?\n il faut regarder sur la grilles et savoir votre postistion et la position de votre énnmie aussi puis bouuuf attaquer");
 
-}
+        } else {
+            printf("on est gooo !");
+
+        }
+    }
+
     return 0;
+
 }
